2da15fd13c320ef50396f4cf45473635aa7bf3e1
Johanna Linkemeyer <jlinkemeyer@uos.de>
2020-07-06 22:09:15 +0200

    alle combinations fertig

--- 04_trials.js
+++ 04_trials.js
@@ -3,34 +3,40 @@
 let correctLabelCombinations = [];
 let congruentSoundCombinations = [];
 let incongruentSoundCombinations = [];
-let incorrectCombinations = [];
+let incorrectLabelCombinations = [];
+let incorrectSoundCombinations = [];
 
+// CREATING ARRAYS THAT CONTAIN STRINGS DESCRIBING PATHS OF ALL LABELS, SOUNDS, AND IMAGES
+
+// Implementing it this way, it would be quite easy to add further categories. This is not
+// necessary for our experiment - however it is nice to have logically created path-names
+// as this simplifies coding the possible correct and incorrect combinations
 let categories = ["bird", "drum", "motorcycle", "dog", "phone", "guitar"];
 let subcategory = [["owl", "sparrow"], ["bongo", "kit"], ["big", "small"], ["labmix", "poodle"], ["cell", "rotary"], ["electric", "acoustic"]];
 let gender = ["female", "male"];
 let index = ["1", "1", "2", "2"];
 
-let labelString = [];
-let imageString = [];
-let soundString = [];
 
 // Creates Array containing all label paths
-for (let i1 = 0; i1 < 6; i1++) {
-    for (let i2 = 0; i2 < 2; i2++) {
-        labelString.push("labels/" + categories[i1] + "_" + gender[i2] + ".wav");
+let labelPaths = [];
+for (let i1 = 0; i1 < categories.length; i1++) {
+    for (let i2 = 0; i2 < gender.length; i2++) {
+        labelPaths.push("labels/" + categories[i1] + "_" + gender[i2] + ".wav");
     }
 }
 
 // Creates Array containing all image paths
-for (let j = 0; j < 24; j++) {
+let imagePaths = [];
+for (let j = 0; j < categories.length*4; j++) {
     let cat = Math.round((j/4)-0.5);
-    imageString.push("images/" + categories[cat] + "_" + subcategory[cat][j%2] + "_" + index[j%4] + ".jpg");
+    imagePaths.push("images/" + categories[cat] + "_" + subcategory[cat][j%2] + "_" + index[j%4] + ".jpg");
 }
 
 // Creates Array containing all sound paths
-for (let k1 = 0; k1 < 6; k1++) {
-    for (let k2 = 0; k2 < 2; k2++){
-        soundString.push("sounds/" + categories[k1] + "_" + subcategory[k1][k2] + ".wav");
+let soundPaths = [];
+for (let k1 = 0; k1 < subcategory.length; k1++) {
+    for (let k2 = 0; k2 < subcategory[k1].length; k2++){
+        soundPaths.push("sounds/" + categories[k1] + "_" + subcategory[k1][k2] + ".wav");
     }
 }
 
@@ -39,27 +45,24 @@ for (let k1 = 0; k1 < 6; k1++) {
 // correct match -> 48 combinations (2 labels per category, 4 pictures per category)
 let x3;
 let x4;
-let counter;
 
 // Find correct combination for each of the labelString-entries:
-for (let x1 = 0; x1 < labelString.length; x1++) {
+for (let x1 = 0; x1 < labelPaths.length; x1++) {
     x3 = Math.round((x1/2)-0.5);
-    counter = 0;
     
     // TODO das dauert, redundanter code, nochmal drueberschauen fuer bessere Idee
     for (let x2 = 0; x2 < 24; x2++) {
         x4 = Math.round((x2/4)-0.5);
         
         // if label and image are a "matching" combination, push them to the array
-        if (x3 == x4) {
-            correctLabelCombinations.push([labelString[x1], imageString[x2]]);
-            counter += 1;
-        }
-        
-        // inner for-loop can be terminated early once 4 combinations for each label 
-        // have been found
-        if (counter >= 4) {
-            break;
+        if (x3 === x4) {
+            
+            correctLabelCombinations.push([labelPaths[x1], imagePaths[x2]]);
+            
+        } else { 
+            
+            // all incorrect label-image combinations (240)
+            incorrectLabelCombinations.push([labelPaths[x1], imagePaths[x2]]);
         }
         
     }
@@ -69,112 +72,157 @@ for (let x1 = 0; x1 < labelString.length; x1++) {
 let imagePosCon;
 let imagePosIncon;
 
-for (let y1 = 0; y1 < soundString.length; y1++) {
+for (let y1 = 0; y1 < soundPaths.length; y1++) {
     imagePosCon = y1*2;
     imagePosIncon = imagePosCon;
     
-    if (y1 % 2 == 1) {
+    if (y1 % 2 === 1) {
         imagePosCon -= 1;
     } else {
         imagePosIncon += 3;
     }
-
-    // all congruent combinations
-    congruentSoundCombinations.push([soundString[y1], imageString[imagePosCon]]);
-    congruentSoundCombinations.push([soundString[y1], imageString[imagePosCon+2]]);
-
-    // all incongruent combinations
-    incongruentSoundCombinations.push([soundString[y1], imageString[imagePosIncon-2]]);
-    incongruentSoundCombinations.push([soundString[y1], imageString[imagePosIncon]]);
+        
+    for (let y2 = 0; y2 < imagePaths.length; y2++) {
+        
+        if (y2 === imagePosCon || y2 === imagePosCon+2) {
+            
+            // all congruent combinations
+            congruentSoundCombinations.push([soundPaths[y1], imagePaths[y2]]);
+            
+        } else if (y2 === imagePosIncon || y2 === imagePosIncon-2) {
+            
+            // all incongruent combinations
+            incongruentSoundCombinations.push([soundPaths[y1], imagePaths[y2]]);
+            
+        } else {
+            
+            // all incorrect sound-image combinations (240)
+            incorrectSoundCombinations.push([soundPaths[y1], imagePaths[y2]]);
+        }
+    }
 }
+
+// Function that takes an array and shuffles it (this function is used to avoid 
+// regularities in the incorrect trials)
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+
+// Only 48 incorrect label-image and 48 incorrect sound-image combinations needed.
+// Shuffle arrays (240 entries each) first, then slice such that 48 combinations 
+// remain in each array.
+shuffle(incorrectLabelCombinations);
+incorrectLabelCombinations = incorrectLabelCombinations.slice(40,88);
+
+shuffle(incorrectSoundCombinations);
+incorrectSoundCombinations = incorrectSoundCombinations.slice(40,88);
+
+
+// TODO: Das ist nur zum Testen!!
+// ------------------------------------------------------------------------------------
+for(let i = 0; i < incongruentSoundCombinations.length; i++) {
+    //console.log(incongruentSoundCombinations[i]);
+}
+for(let i = 0; i < congruentSoundCombinations.length; i++) {
+    //console.log(congruentSoundCombinations[i]);
+}
+for(let i = 0; i < correctLabelCombinations.length; i++) {
+    //console.log(correctLabelCombinations[i]);
+}
+for(let i = 0; i < incorrectLabelCombinations.length; i++) {
+    //console.log(incorrectLabelCombinations[i]);
+}
+console.log(congruentSoundCombinations[0][0]);
+console.log(congruentSoundCombinations[0][1]);
+
+// ------------------------------------------------------------------------------------
+
 
 const practice_trials = {
     key_press: [
         {
-            picture: "images/practice/13_50_same.jpg",
-            // ----------------------------------------
-            sound: "sounds/guitar_acoustic.wav",
-            // ----------------------------------------
+            picture: congruentSoundCombinations[0][0],
+            sound: congruentSoundCombinations[0][1],
             item: 13,
             expected: "same",
             angle: 50,
         },
         {
-            picture: "images/practice/13_50_different.jpg",
-            sound: "sounds/guitar_acoustic.wav",
+            picture: congruentSoundCombinations[1][0],
+            sound: congruentSoundCombinations[1][1],
             item: 13,
-            expected: "different",
-            angle: 50,
-        },
-        {
-            picture: "images/practice/13_150_same.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 13,
-            expected: "same",
-            angle: 150,
-        },
-        {
-            picture: "images/practice/13_150_different.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 13,
-            expected: "different",
-            angle: 150,
-        },
-        {
-            picture: "images/practice/14_50_same.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 14,
             expected: "same",
             angle: 50,
         },
         {
-            picture: "images/practice/14_50_different.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 14,
-            expected: "different",
+            picture: congruentSoundCombinations[2][0],
+            sound: congruentSoundCombinations[2][1],
+            item: 13,
+            expected: "same",
             angle: 50,
         },
         {
-            picture: "images/practice/14_150_same.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 14,
+            picture: congruentSoundCombinations[3][0],
+            sound: congruentSoundCombinations[3][1],
+            item: 13,
             expected: "same",
-            angle: 150
+            angle: 50,
         },
         {
-            picture: "images/practice/14_150_different.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 14,
-            expected: "different",
-            angle: 150
-        },
-        {
-            picture: "images/practice/15_50_same.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 15,
+            picture: congruentSoundCombinations[4][0],
+            sound: congruentSoundCombinations[4][1],
+            item: 13,
             expected: "same",
-            angle: 50
+            angle: 50,
         },
         {
-            picture: "images/practice/15_50_different.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 15,
-            expected: "different",
-            angle: 50
-        },
-        {
-            picture: "images/practice/15_150_same.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 15,
+            picture: congruentSoundCombinations[5][0],
+            sound: congruentSoundCombinations[5][1],
+            item: 13,
             expected: "same",
-            angle: 150
+            angle: 50,
         },
         {
-            picture: "images/practice/15_150_different.jpg",
-            sound: "sounds/guitar_acoustic.wav",
-            item: 15,
-            expected: "different",
-            angle: 150
+            picture: congruentSoundCombinations[6][0],
+            sound: congruentSoundCombinations[6][1],
+            item: 13,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: congruentSoundCombinations[7][0],
+            sound: congruentSoundCombinations[7][1],
+            item: 13,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: congruentSoundCombinations[8][0],
+            sound: congruentSoundCombinations[8][1],
+            item: 13,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: congruentSoundCombinations[9][0],
+            sound: congruentSoundCombinations[9][1],
+            item: 13,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: congruentSoundCombinations[10][0],
+            sound: congruentSoundCombinations[10][1],
+            item: 13,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: congruentSoundCombinations[11][0],
+            sound: congruentSoundCombinations[11][1],
+            item: 13,
+            expected: "same",
+            angle: 50,
         },
     ],
 };
